030a8115bc2914ec29e291f8586fe63c3090b4ef
AlexStasiuk <sov853972@gmail.com>
2021-11-24 10:24:39 +0200

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import App from "./components/app/app";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route
-        path="/yalantis-test-task/"
-        element={<Navigate to="/yalantis-test-task/employees/1" />}
-      />
-      <Route path="/yalantis-test-task/employees/" element={<App />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
